e44e44a6a4595d3976e6e3865d393476c79901dc
mongsjob <mongodblogs122@hotmail.com>
2025-04-14 09:57:05 +0100

    Update authApi.js

--- src/redux/feature/auth/authApi.js
+++ src/redux/feature/auth/authApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const authApi = createApi({
     reducerPath: 'authApi',
     baseQuery: fetchBaseQuery({ 
-        baseUrl: "https://products-server-98j0.onrender.com/api/auth",
+        baseUrl: "https://products-backend-djb0.onrender.com/api/auth",
         credentials: 'include' 
     }),
     endpoints: (builder)=>({
